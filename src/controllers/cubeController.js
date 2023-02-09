c90a5dcedec314e399f33fed33e93ab00123c77a
Nikolay Cherepov <114342845+aeconick@users.noreply.github.com>
2023-02-09 15:45:28 +0200

    Attach accessory to cube

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -30,3 +30,14 @@ exports.getAttachAccessory = async (req, res) => {
     const accessories = await Accessory.find().lean();
     res.render('cube/attach', { cube, accessories });
 };
+
+exports.postAttachAccessory = async (req, res) => {
+    const cube = await Cube.findById(req.params.cubeId);
+    const accessoryId = req.body.accessory;
+    console.log(accessoryId);
+    cube.accessories.push(accessoryId);
+
+    cube.save();
+
+    res.redirect(`/cubes/${cube._id}/details`);
+};
