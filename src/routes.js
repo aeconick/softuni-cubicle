eeb9151aa1e7b6742c8f61c43656e9885f073fba
Nikolay Cherepov <114342845+aeconick@users.noreply.github.com>
2023-02-09 14:08:59 +0200

    Add new resources
    Add accessory controller

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeControllers');
+const accessoryController = require('./controllers/accessoryController');
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
@@ -10,6 +11,8 @@ router.get('/404', homeController.getErrorPage);
 router.get('/create', cubeController.getCreateCube);
 router.post('/create', cubeController.postCreateCube);
 router.get('/details/:cubeId', cubeController.getDetails);
+
+router.use('/accessory', accessoryController);
 
 
 module.exports = router;
