f6578fe79c02e26b195f15105e0b23e558a0927b
Nikolay Cherepov <114342845+aeconick@users.noreply.github.com>
2023-02-13 23:01:56 +0200

    Check user and password on login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,6 +5,19 @@ router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await authService.login(username, password);
+    } catch (err) {
+        console.log(err);
+
+        return res.redirect('/');
+    }
+
+    res.redirect('/');
+});
+
 router.get('/register', (req, res) => {
     res.render('auth/register');
 });
@@ -22,7 +35,7 @@ router.post('/register', async (req, res) => {
         return res.redirect('404');
     }
 
-    const user = await authService.register(username,password);
+    const user = await authService.register(username, password);
 
     res.redirect('/login');
 });
